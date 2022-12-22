a22dc4e302480872be5739c461ffd3c413e6058d
raphael720 <raphaelleite720@gmail.com>
2022-12-22 11:19:01 -0300

    criando imagem png

--- src/main.py
+++ src/main.py
@@ -1,34 +1,59 @@
 import math
 import os
 
-# image
-image_colors = 'P3'
-image_width = 256
-image_height = 256
+from PIL import Image
 
-if 'images' not in os.listdir(os.path.join(os.path.dirname(__file__), '..')):
-    os.mkdir(os.path.join(os.path.dirname(__file__), '..', 'images'))
+def path_image(title:str, extension:str) -> str:
+    path = os.path.join(os.path.dirname(__file__), '..', 'images', f'{title}.{extension}')
 
-path_image = lambda title: os.path.join(os.path.dirname(__file__), '..', 'images', f'{title}.ppm')
 
-# PPM image
-title = 'image_0'
-file = open(path_image(title), 'w')
+if __name__ == "__main__":
+    # image
+    title = 'image_0'
+    image_colors = 'P3' # para o formato ppm
+    image_width = 800
+    image_height = 600
 
-file.write(f'{title}\n')
-file.write(f'{image_width} {image_height}\n')
-file.write('255\n')
+    if 'images' not in os.listdir(os.path.join(os.path.dirname(__file__), '..')):
+        os.mkdir(os.path.join(os.path.dirname(__file__), '..', 'images'))
 
-for j in range(image_height-1, -1, -1):
-    for i in range(0, image_width):
-        r = i / (image_width-1)
-        g = j / (image_height-1)
-        b = 0.25
+    # PPM image
+    
+    file = open(path_image(title, 'ppm'), 'w')
 
-        ir = int(255.99 * r)
-        ig = int(255.99 * g)
-        ib = int(255.99 * b)
+    file.write(f'{image_colors}\n')
+    file.write(f'{image_width} {image_height}\n')
+    file.write('255\n')
 
-        file.write(f'{ir} {ig} {ib}\n')
+    for j in range(image_height-1, -1, -1):
+        for i in range(0, image_width):
+            r = i / (image_width-1)
+            g = j / (image_height-1)
+            b = 0.25
 
-file.close()
+            ir = int(255.99 * r)
+            ig = int(255.99 * g)
+            ib = int(255.99 * b)
+
+            file.write(f'{ir} {ig} {ib}\n')
+
+    file.close()
+
+    #PNG image
+    image_png = Image.new('RGB', (image_width, image_height))
+    image_pixel = image_png.load()
+
+    for j in range(0, image_height):
+        for i in range(0, image_width):
+            r = i / (image_width-1)
+            g = 1 - j / (image_height-1)
+            b = 0.25
+
+            ir = int(255.99 * r)
+            ig = int(255.99 * g)
+            ib = int(255.99 * b)
+
+            image_pixel[i, j] = (ir,ig,ib)
+
+
+    image_png.save(path_image(title, 'png'), "PNG")
